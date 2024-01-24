d8eefff42c0ba950e3b140f0d9dcf80fb740a66e
Vadim <vadimx30@gmail.com>
2024-01-24 21:14:46 +0300

    Done. M6:task4

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,6 +1,6 @@
 from django.http import Http404, HttpResponseNotFound
 from django.shortcuts import render, redirect
-from MainApp.forms import SnippetForm
+from MainApp.forms import SnippetForm, UserRegistrationForm
 from MainApp.models import Snippet
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib import auth
@@ -100,6 +100,24 @@ def snippet_delete(request, snippet_id):
         return HttpResponseNotFound(f'Snippet with id={snippet_id} not found')  
     snippet.delete()
     return redirect('snippets-list')
+
+
+def create_user(request):
+    context = {'pagename': "Регистрация пользователя"}
+    # Пустая форма для заполнения данных
+    if request.method == "GET":
+        form = UserRegistrationForm()
+        context['form'] = form
+        return render(request, "pages/registration.html", context)
+
+    # Используем данные из формы
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        context['form'] = form
+        return render(request, "pages/registration.html", context)
 
 
 def login(request):
